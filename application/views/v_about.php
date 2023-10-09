20b0a6f5dbfa9e9ca04eeb98b38cb66c0661142c
rickyroberto22 <rickyroberto57@gmail.com>
2023-10-09 20:20:05 +0700

    Pembuatan halaman depan dan about

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Imam</li>
+        <li>Nama Belakang : Sugiman</li>
+        <li>Nama Panjang : Imam Bonjol</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Ciledug Raya No. 012168 Pesanggrahan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Dikebon</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
